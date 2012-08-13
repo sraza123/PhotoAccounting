59ecdfaa0bc7b16e7d01974d13f985acaeeb45bf
iliketosneeze <szikora0531@yahoo.de>
2012-08-13 22:51:20 +0200

    Revert "Only the last released key has effect"
    This reverts commit 15c3b92f27082e51bb0adcf927b5223436c7efc7.

--- jviewer.js
+++ jviewer.js
@@ -14,7 +14,6 @@ JSViewer = function () {
     cache_group = 1;
     image_index = 0; // keep track of image being cached
     current_image_index = 0; // keep track of the current image being viewed
-    current_pressed_keys = 0;
 
     /**
      * Creates a shortcut for document.getElementById
@@ -132,15 +131,6 @@ JSViewer = function () {
     keyDownHandler = function (Y, total_number_images, POST_CACHE, PRE_CACHE) {
         return function (e) {
             e.preventDefault();
-            
-            var valas = my_key_codes[String.fromCharCode(e.keyCode).toLowerCase()];
-            if(valas != undefined) {
-                $('jsv_konto').value = valas;
-                //comment out the next line if you want the cursor to stay in the field.
-                $('jsv_konto').blur();
-                current_pressed_keys++;
-            
-            }
 
             switch (e.keyCode) {
             case 13: // enter
@@ -167,14 +157,11 @@ JSViewer = function () {
         return function (e) {
             e.preventDefault();
 
-            var valas = my_key_codes[String.fromCharCode(e.keyCode).toLowerCase()];
+            var valas = my_codes[String.fromCharCode(e.keyCode).toLowerCase()];
 
             if (valas != undefined) {
-                current_pressed_keys--;
-                if(current_pressed_keys == 0) {
-                    current_image_index++;
-                    renderImage(Y, total_number_images, localStorage.getItem('img-' + current_image_index));
-                }
+                current_image_index++;
+                renderImage(Y, total_number_images, localStorage.getItem('img-' + current_image_index));
             }
 
             /*switch (e.keyCode) {
@@ -196,7 +183,7 @@ JSViewer = function () {
      * @return {null} 
      */
     setKeyboardHandlers = function (Y, total_number_images, POST_CACHE, PRE_CACHE) {
-        Y.one('doc').on("keydown", keyDownHandler(Y, total_number_images, POST_CACHE, PRE_CACHE));
+        Y.one('doc').on("key", keyDownHandler(Y, total_number_images, POST_CACHE, PRE_CACHE), 'enter,81,87');
         Y.one('doc').on("keyup", keyUpHandler(Y, total_number_images, POST_CACHE, PRE_CACHE));
     };
 
