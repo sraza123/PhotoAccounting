892712fd7cdbfd2fde87ccc302e8b3399379d0b7
peter-govind-jr <peter.govind.jr@gmail.com>
2012-08-14 21:18:46 +0800

    Clarify comments on table sql and sample data

--- image_detail.php
+++ image_detail.php
@@ -3,7 +3,7 @@
 -- -- The SQL to create the table in PostgreSQL is as follows :
 CREATE TABLE entries (id SERIAL PRIMARY KEY, customer_id INT, entry_id INT, image_id INT, entry_date DATE, text VARCHAR(9999), amount NUMERIC(20, 2), account INT, offset_account INT);
 
--- -- The insert sample data like so :
+-- -- Then insert sample data like so :
 INSERT INTO entries (customer_id, entry_id, image_id, entry_date, text, amount, account, offset_account) VALUES (1,1,1,'2012-08-14','TEST 1',1,1,1);
 INSERT INTO entries (customer_id, entry_id, image_id, entry_date, text, amount, account, offset_account) VALUES (2,2,2,'2012-08-14','TEST 2',2,2,2);
 INSERT INTO entries (customer_id, entry_id, image_id, entry_date, text, amount, account, offset_account) VALUES (5,5,5,'2012-08-14','TEST 5',5,5,5);
@@ -36,6 +36,6 @@ pg_free_result($result);
 // Closing connection
 pg_close($dbconn);
 
-// Print results
+// Print results in JSON
 echo json_encode($detail);
 ?>
